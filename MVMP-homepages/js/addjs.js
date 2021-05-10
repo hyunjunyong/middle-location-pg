fcb6b8a9f86e87378a1f9c36a7ca5199ef8ef524
유은지 <aine0227@naver.com>
2021-05-10 17:08:52 +0900

    addevenlistener 오류난거 수정

--- MVMP-homepages/js/addjs.js
+++ MVMP-homepages/js/addjs.js
@@ -1,12 +1,9 @@
 const $add1 = document.getElementById('add1');
 const $add2 = document.getElementById('add2');
-const $btn1 = document.getElementById('button-addon1');
-const $btn2 = document.getElementById('button-addon2');
+const $btn1 = document.getElementById('search-button1');
+const $btn2 = document.getElementById('search-button2');
 const url = "https://dapi.kakao.com/v2/local/search/address.json";
 const headers = { Authorization: " KakaoAK 9434c60fa9c26e7c4f5c81801f763f04" };
-
-$btn1.addEventListener("click", ()=>searchaddname($add1));
-$btn2.addEventListener("click", ()=>searchaddname($add2))
 
 //지도
 var mapContainer = document.getElementById('map'),
@@ -15,8 +12,13 @@ var mapContainer = document.getElementById('map'),
         level: 3
     };
 
+
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption);
+
+
+$btn1.addEventListener("click", ()=>searchaddname($add1));
+$btn2.addEventListener("click", ()=>searchaddname($add2));
 
 //주소 검색시 json출력
 async function searchaddname($add) {
@@ -31,6 +33,8 @@ async function searchaddname($add) {
     const json = await data;
     drawMarker(json);
 }
+
+
 
 function drawMarker(json) {
     console.log(json)
@@ -53,7 +57,7 @@ function drawMarker(json) {
 
             // 인포윈도우로 장소에 대한 설명을 표시합니다
             var infowindow = new kakao.maps.InfoWindow({
-                content: '<div style="width:150px;text-align:center;padding:6px 0;">나의 위치</div>'
+                content: '<div style="width:150px;text-align:center;padding:6px 0;">위치</div>'
             });
             infowindow.open(map, marker);
 
@@ -63,15 +67,31 @@ function drawMarker(json) {
     });
 }
 
+
 //더보기 버튼 클릭시 주소 검색 폼 추가
-const $addressFormBtn = document.querySelector(".address-form-group");
-const $moreBtn = document.getElementById("more-button");
+// const $addressFormBtn = document.querySelector(".address-form-group");
+// const $moreBtn = document.getElementById("more-button");
 
-$moreBtn.addEventListener("click", makeForm);
-
-function makeForm(){
-    const div = document.createElement("div");
-    div.setAttribute('className', 'input-group mb-3');
-    div.innerHTML ='<input type="text" id=\'add1\' className="form-control" aria-label="Recipient\'s username"aria-describedby="button-addon2"> <button className="btn btn-outline-secondary" type="button" id="button-addon1">검색</button>'
-    $addressFormBtn.appendChild(div);
-}
+// $moreBtn.addEventListener("click", makeForm);
+//
+// function makeForm(){
+//     const div = document.createElement("div");
+//     div.setAttribute('className', 'input-group mb-3');
+//     const input = document.createElement("input");
+//     input.setAttribute("type", "text");
+//     input.setAttribute("className", "form-control");
+//     input.setAttribute("aria-label", "Recipient's username");
+//     input.setAttribute("aria-describedby", "button-addon2");
+//     const button = document.createElement('button');
+//     button.setAttribute("className", "btn btn-outline-secondary search=btn");
+//     button.setAttribute("type", "button");
+//     const searchTextNode = document.createTextNode("검색");
+//     button.appendChild(searchTextNode);
+//
+//     div.appendChild(input);
+//     div.appendChild(button);
+//
+//     $addressFormBtn.appendChild(div);
+//     const $addedBtn = document.querySelectorAll(".search-btn");
+//     console.log($addedBtn)
+// }
