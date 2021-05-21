46906dfb119b86cb448c2cf1be48ffa11d34a087
유은지 <aine0227@naver.com>
2021-05-21 18:27:43 +0900

    위도경도 로컬스토리지에 저장 및 리턴

--- js/addjs.js
+++ js/addjs.js
@@ -66,59 +66,62 @@ function nowPosition() {
 
 
 //지도
-    var mapContainer = document.getElementById('map'),
-        mapOption = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-            level: 3
-        };
+var mapContainer = document.getElementById('map'),
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3
+    };
 
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-    var map = new kakao.maps.Map(mapContainer, mapOption);
+var map = new kakao.maps.Map(mapContainer, mapOption);
 
 //주소 검색시 json출력
-    async function searchaddname($add) {
-        const addValue = $add.value;
-        if (addValue === '') {
-            alert('주소를 입력하세요');
-            return false;
+async function searchaddname($add) {
+    const addValue = $add.value;
+    if (addValue === '') {
+        alert('주소를 입력하세요');
+        return false;
+    }
+
+    const respone = await fetch(`${url}?query=${addValue}`, {headers});
+    const data = await respone.json();
+    const json = await data;
+    drawMarker(json);
+}
+
+function drawMarker(json) {
+    console.log(json)
+    // 주소-좌표 변환 객체를 생성합니다
+    var geocoder = new kakao.maps.services.Geocoder();
+
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(json.documents[0].address_name, function (result, status) {
+
+        // 정상적으로 검색이 완료됐으면
+        if (status === kakao.maps.services.Status.OK) {
+
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: coords
+            });
+
+            // 인포윈도우로 장소에 대한 설명을 표시합니다
+            var infowindow = new kakao.maps.InfoWindow({
+                content: '<div style="width:150px;text-align:center;padding:6px 0;">위치</div>'
+            });
+            infowindow.open(map, marker);
+
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+
+            //로컬스토리지에 위도 경도 저장
+            savePosition(marker.getPosition());
         }
-
-        const respone = await fetch(`${url}?query=${addValue}`, {headers});
-        const data = await respone.json();
-        const json = await data;
-        drawMarker(json);
-    }
-
-    function drawMarker(json) {
-        console.log(json)
-        // 주소-좌표 변환 객체를 생성합니다
-        var geocoder = new kakao.maps.services.Geocoder();
-
-        // 주소로 좌표를 검색합니다
-        geocoder.addressSearch(json.documents[0].address_name, function (result, status) {
-
-            // 정상적으로 검색이 완료됐으면
-            if (status === kakao.maps.services.Status.OK) {
-
-                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-                // 결과값으로 받은 위치를 마커로 표시합니다
-                var marker = new kakao.maps.Marker({
-                    map: map,
-                    position: coords
-                });
-
-                // 인포윈도우로 장소에 대한 설명을 표시합니다
-                var infowindow = new kakao.maps.InfoWindow({
-                    content: '<div style="width:150px;text-align:center;padding:6px 0;">위치</div>'
-                });
-                infowindow.open(map, marker);
-
-                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-                map.setCenter(coords);
-            }
-        });
-    }
+    });
+}
 
 //더보기 버튼 클릭시 주소 검색 폼 추가
 const $addressFormBtn = document.querySelector(".address-form-group");
@@ -135,3 +138,21 @@ function makeForm() {
     $addedSearchBtn.addEventListener("click", ()=>searchaddname($addedInput));
 }
 
+function savePosition(adrress){
+
+    let isExist = localStorage.getItem("positions");
+    isExist = isExist ? JSON.parse(isExist): [];
+
+    //address = {위도: 111, 경도: 222}
+    // const adrress = {'x': x, 'y': y};
+    isExist.push(adrress)
+
+    localStorage.setItem("positions", JSON.stringify(isExist))
+}
+
+$midBtn = document.getElementById("mid-btn");
+$midBtn.addEventListener("click", getPositions);
+
+function getPositions(){
+    return localStorage.getItem('positions');
+}
