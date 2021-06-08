129376dbea2318527f009f256f051019b110b597
유은지 <aine0227@naver.com>
2021-06-09 04:04:02 +0900

    버스테스트1

--- js/contact.js
+++ js/contact.js
@@ -182,3 +182,17 @@ function makeOutListener(infowindow) {
     infowindow.close();
   };
 }
+
+function searchBusLaneAJAX() {
+  var xhr = new XMLHttpRequest();
+  var url = "https://api.odsay.com/v1/api/searchBusLane?busNo=360&CID=8000&apiKey={LRP8InDDBglP/04OezKdyA}";
+  xhr.open("GET", url, true);
+  xhr.send();
+  xhr.onreadystatechange = function() {
+
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log( xhr.responseText ); // <- xhr.responseText 로 결과를 가져올 수 있음
+    }
+  }
+}
+searchBusLaneAJAX();
