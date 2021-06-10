e93b6ca10cb5ec01a1dfe82aadcab32b0d3f2323
유은지 <aine0227@naver.com>
2021-06-11 02:19:49 +0900

    버스 툴 만들기 (8)

--- js/contact.js
+++ js/contact.js
@@ -221,13 +221,13 @@ function searchBusLaneAJAX() {
       let $payment= document.getElementsByClassName('payment');
       let $totalDistance= document.getElementsByClassName('totalDistance');
 
-      // let $totalTime_V = busPath.result.path[0].info.totalTime;
+       let $totalTime_V = busPath.result.path[0].info.totalTime;
       // let $totalWalk_V = busPath.result.path[0].info.totalWalk;
       // let $payment_V = busPath.result.path[0].info.payment
       // let $totalDistance_V= busPath.result.path[0].info.totalDistance;
 
 
-      $totalTime.innerHTML = busPath.result.path[0].info.totalTime;
+      // $totalTime.innerHTML = busPath.result.path[0].info.totalTime;
       // $totalWalk.innerHTML = $totalWalk_V;
       // $payment.innerHTML = $payment_V;
       // $totalDistance.innerHTML = $totalDistance_V;
