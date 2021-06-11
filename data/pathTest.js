0e38b5fcc6d1fce8487d54f8879600d50613f6e0
유은지 <aine0227@naver.com>
2021-06-12 05:28:24 +0900

    버스 툴 만들기(32-2)

---
+++ data/pathTest.js
@@ -0,0 +1,454 @@
+export default {
+    "result": {
+        "searchType": 0,
+        "outTrafficCheck": 0,
+        "busCount": 4,
+        "subwayCount": 0,
+        "subwayBusCount": 0,
+        "pointDistance": 2812,
+        "startRadius": 700,
+        "endRadius": 700,
+        "path": [{
+            "pathType": 2,
+            "info": {
+                "trafficDistance": 3465.0,
+                "totalWalk": 839,
+                "totalTime": 29,
+                "payment": 1150,
+                "busTransitCount": 1,
+                "subwayTransitCount": 0,
+                "mapObj": "2170927:1:0:9",
+                "firstStartStation": "제주대학교",
+                "lastEndStation": "아라동주민센터",
+                "totalStationCount": 9,
+                "busStationCount": 9,
+                "subwayStationCount": 0,
+                "totalDistance": 4304.0,
+                "totalWalkTime": -1
+            },
+            "subPath": [{"trafficType": 3, "distance": 402, "sectionTime": 6}, {
+                "trafficType": 2,
+                "distance": 3465,
+                "sectionTime": 16,
+                "stationCount": 9,
+                "lane": [{"busNo": "365(한라대)", "type": 11, "busID": 2170927}, {
+                    "busNo": "360(제주고)",
+                    "type": 11,
+                    "busID": 2170924
+                }, {"busNo": "345(제주대.한마음병원.삼화지구.한마음병원.제주대)", "type": 11, "busID": 2170889}, {
+                    "busNo": "342(함덕(신사동))",
+                    "type": 11,
+                    "busID": 2170872
+                }, {"busNo": "355(수산)", "type": 11, "busID": 2170906}, {
+                    "busNo": "356(제주대.연삼로.수산)",
+                    "type": 11,
+                    "busID": 2170911
+                }, {"busNo": "351(봉개)", "type": 11, "busID": 2170895}, {
+                    "busNo": "341(함덕(신사동))",
+                    "type": 11,
+                    "busID": 2170866
+                }, {"busNo": "270(애월하나로마트)", "type": 11, "busID": 2170768}, {
+                    "busNo": "352(봉개)",
+                    "type": 11,
+                    "busID": 2170901
+                }],
+                "startName": "제주대학교",
+                "startX": 126.561648,
+                "startY": 33.460098,
+                "endName": "아라동주민센터",
+                "endX": 126.544915,
+                "endY": 33.475951,
+                "startID": 800206,
+                "endID": 800133,
+                "passStopList": {
+                    "stations": [{
+                        "index": 0,
+                        "stationID": 800206,
+                        "stationName": "제주대학교",
+                        "x": "126.561648",
+                        "y": "33.460098",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 1,
+                        "stationID": 800208,
+                        "stationName": "제대아파트",
+                        "x": "126.55729",
+                        "y": "33.458902",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 2,
+                        "stationID": 800210,
+                        "stationName": "제대마을",
+                        "x": "126.553941",
+                        "y": "33.457853",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 3,
+                        "stationID": 800723,
+                        "stationName": "제주대학교 입구",
+                        "x": "126.551001",
+                        "y": "33.456755",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 4,
+                        "stationID": 800116,
+                        "stationName": "남국사",
+                        "x": "126.549556",
+                        "y": "33.462157",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 5,
+                        "stationID": 800118,
+                        "stationName": "웃 인다 마을",
+                        "x": "126.549259",
+                        "y": "33.464479",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 6,
+                        "stationID": 800122,
+                        "stationName": "제주대학교병원",
+                        "x": "126.547782",
+                        "y": "33.469177",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 7,
+                        "stationID": 800124,
+                        "stationName": "인다마을",
+                        "x": "126.545874",
+                        "y": "33.470697",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 8,
+                        "stationID": 800126,
+                        "stationName": "아라주공아파트",
+                        "x": "126.545791",
+                        "y": "33.473085",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 9,
+                        "stationID": 800133,
+                        "stationName": "아라동주민센터",
+                        "x": "126.544915",
+                        "y": "33.475951",
+                        "isNonStop": "N"
+                    }]
+                }
+            }, {"trafficType": 3, "distance": 437, "sectionTime": 7}]
+        }, {
+            "pathType": 2,
+            "info": {
+                "trafficDistance": 3465.0,
+                "totalWalk": 839,
+                "totalTime": 29,
+                "payment": 1150,
+                "busTransitCount": 1,
+                "subwayTransitCount": 0,
+                "mapObj": "2171283:1:0:9",
+                "firstStartStation": "제주대학교",
+                "lastEndStation": "아라동주민센터",
+                "totalStationCount": 9,
+                "busStationCount": 9,
+                "subwayStationCount": 0,
+                "totalDistance": 4304.0,
+                "totalWalkTime": -1
+            },
+            "subPath": [{"trafficType": 3, "distance": 402, "sectionTime": 6}, {
+                "trafficType": 2,
+                "distance": 3465,
+                "sectionTime": 16,
+                "stationCount": 9,
+                "lane": [{"busNo": "455(장전)", "type": 12, "busID": 2171283}, {
+                    "busNo": "447(도평)",
+                    "type": 12,
+                    "busID": 2171189
+                }],
+                "startName": "제주대학교",
+                "startX": 126.561648,
+                "startY": 33.460098,
+                "endName": "아라동주민센터",
+                "endX": 126.544915,
+                "endY": 33.475951,
+                "startID": 800206,
+                "endID": 800133,
+                "passStopList": {
+                    "stations": [{
+                        "index": 0,
+                        "stationID": 800206,
+                        "stationName": "제주대학교",
+                        "x": "126.561648",
+                        "y": "33.460098",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 1,
+                        "stationID": 800208,
+                        "stationName": "제대아파트",
+                        "x": "126.55729",
+                        "y": "33.458902",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 2,
+                        "stationID": 800210,
+                        "stationName": "제대마을",
+                        "x": "126.553941",
+                        "y": "33.457853",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 3,
+                        "stationID": 800723,
+                        "stationName": "제주대학교 입구",
+                        "x": "126.551001",
+                        "y": "33.456755",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 4,
+                        "stationID": 800116,
+                        "stationName": "남국사",
+                        "x": "126.549556",
+                        "y": "33.462157",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 5,
+                        "stationID": 800118,
+                        "stationName": "웃 인다 마을",
+                        "x": "126.549259",
+                        "y": "33.464479",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 6,
+                        "stationID": 800122,
+                        "stationName": "제주대학교병원",
+                        "x": "126.547782",
+                        "y": "33.469177",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 7,
+                        "stationID": 800124,
+                        "stationName": "인다마을",
+                        "x": "126.545874",
+                        "y": "33.470697",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 8,
+                        "stationID": 800126,
+                        "stationName": "아라주공아파트",
+                        "x": "126.545791",
+                        "y": "33.473085",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 9,
+                        "stationID": 800133,
+                        "stationName": "아라동주민센터",
+                        "x": "126.544915",
+                        "y": "33.475951",
+                        "isNonStop": "N"
+                    }]
+                }
+            }, {"trafficType": 3, "distance": 437, "sectionTime": 7}]
+        }, {
+            "pathType": 2,
+            "info": {
+                "trafficDistance": 4195.0,
+                "totalWalk": 825,
+                "totalTime": 29,
+                "payment": 1150,
+                "busTransitCount": 1,
+                "subwayTransitCount": 0,
+                "mapObj": "2170952:1:7:15",
+                "firstStartStation": "첨단과기단지 입구 교학촌아파트",
+                "lastEndStation": "제주교통방송 아라아이파크",
+                "totalStationCount": 8,
+                "busStationCount": 8,
+                "subwayStationCount": 0,
+                "totalDistance": 5020.0,
+                "totalWalkTime": -1
+            },
+            "subPath": [{"trafficType": 3, "distance": 689, "sectionTime": 10}, {
+                "trafficType": 2,
+                "distance": 4195,
+                "sectionTime": 17,
+                "stationCount": 8,
+                "lane": [{"busNo": "426(국제부두)", "type": 12, "busID": 2170952}],
+                "startName": "첨단과기단지 입구 교학촌아파트",
+                "startX": 126.571642,
+                "startY": 33.45804,
+                "endName": "제주교통방송 아라아이파크",
+                "endX": 126.549326,
+                "endY": 33.477896,
+                "startID": 803516,
+                "endID": 800566,
+                "passStopList": {
+                    "stations": [{
+                        "index": 0,
+                        "stationID": 803516,
+                        "stationName": "첨단과기단지 입구 교학촌아파트",
+                        "x": "126.571642",
+                        "y": "33.45804",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 1,
+                        "stationID": 803645,
+                        "stationName": "윗가시나물",
+                        "x": "126.569734",
+                        "y": "33.466791",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 2,
+                        "stationID": 802575,
+                        "stationName": "가시나물",
+                        "x": "126.569995",
+                        "y": "33.472528",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 3,
+                        "stationID": 802573,
+                        "stationName": "달샘마을",
+                        "x": "126.570242",
+                        "y": "33.474568",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 4,
+                        "stationID": 800570,
+                        "stationName": "영평상동마을회관",
+                        "x": "126.568247",
+                        "y": "33.476772",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 5,
+                        "stationID": 800571,
+                        "stationName": "영평동",
+                        "x": "126.563268",
+                        "y": "33.479284",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 6,
+                        "stationID": 800562,
+                        "stationName": "신성여자중고등학교",
+                        "x": "126.56134",
+                        "y": "33.478072",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 7,
+                        "stationID": 800617,
+                        "stationName": "금천마을",
+                        "x": "126.553316",
+                        "y": "33.478654",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 8,
+                        "stationID": 800566,
+                        "stationName": "제주교통방송 아라아이파크",
+                        "x": "126.549326",
+                        "y": "33.477896",
+                        "isNonStop": "N"
+                    }]
+                }
+            }, {"trafficType": 3, "distance": 136, "sectionTime": 2}]
+        }, {
+            "pathType": 2,
+            "info": {
+                "trafficDistance": 4726.0,
+                "totalWalk": 538,
+                "totalTime": 26,
+                "payment": 1150,
+                "busTransitCount": 1,
+                "subwayTransitCount": 0,
+                "mapObj": "2170947:1:0:9",
+                "firstStartStation": "제주대학교",
+                "lastEndStation": "제주교통방송 아라아이파크",
+                "totalStationCount": 9,
+                "busStationCount": 9,
+                "subwayStationCount": 0,
+                "totalDistance": 5264.0,
+                "totalWalkTime": -1
+            },
+            "subPath": [{"trafficType": 3, "distance": 402, "sectionTime": 6}, {
+                "trafficType": 2,
+                "distance": 4726,
+                "sectionTime": 18,
+                "stationCount": 9,
+                "lane": [{"busNo": "422(제주대)", "type": 12, "busID": 2170947}],
+                "startName": "제주대학교",
+                "startX": 126.561648,
+                "startY": 33.460098,
+                "endName": "제주교통방송 아라아이파크",
+                "endX": 126.549326,
+                "endY": 33.477896,
+                "startID": 800206,
+                "endID": 800566,
+                "passStopList": {
+                    "stations": [{
+                        "index": 0,
+                        "stationID": 800206,
+                        "stationName": "제주대학교",
+                        "x": "126.561648",
+                        "y": "33.460098",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 1,
+                        "stationID": 800204,
+                        "stationName": "제주모자원입구",
+                        "x": "126.566422",
+                        "y": "33.462839",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 2,
+                        "stationID": 803645,
+                        "stationName": "윗가시나물",
+                        "x": "126.569734",
+                        "y": "33.466791",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 3,
+                        "stationID": 802575,
+                        "stationName": "가시나물",
+                        "x": "126.569995",
+                        "y": "33.472528",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 4,
+                        "stationID": 802573,
+                        "stationName": "달샘마을",
+                        "x": "126.570242",
+                        "y": "33.474568",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 5,
+                        "stationID": 800570,
+                        "stationName": "영평상동마을회관",
+                        "x": "126.568247",
+                        "y": "33.476772",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 6,
+                        "stationID": 800571,
+                        "stationName": "영평동",
+                        "x": "126.563268",
+                        "y": "33.479284",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 7,
+                        "stationID": 800562,
+                        "stationName": "신성여자중고등학교",
+                        "x": "126.56134",
+                        "y": "33.478072",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 8,
+                        "stationID": 800617,
+                        "stationName": "금천마을",
+                        "x": "126.553316",
+                        "y": "33.478654",
+                        "isNonStop": "N"
+                    }, {
+                        "index": 9,
+                        "stationID": 800566,
+                        "stationName": "제주교통방송 아라아이파크",
+                        "x": "126.549326",
+                        "y": "33.477896",
+                        "isNonStop": "N"
+                    }]
+                }
+            }, {"trafficType": 3, "distance": 136, "sectionTime": 2}]
+        }]
+    }
+}
--- js/contact.js
+++ js/contact.js
@@ -101,6 +101,7 @@ function bus() {
             n = i - 1;
             console.log(n);
             searchBusLaneAJAX();
+            // busPathTest();
           });
         }
         //출발지점 마커 그리기
@@ -109,7 +110,7 @@ function bus() {
         var str = result[0].address.address_name
           .fontcolor("navy")
           .bold()
-          .fontsize(2);
+          .fontsize(3);
         var message = "중간위치는 " + str + " 입니다.";
 
         var resultDiv = document.getElementById("clickLatlng");
@@ -212,46 +213,29 @@ function searchBusLaneAJAX() {
       const busPath = JSON.parse(data);
 
       console.log(busPath);
-      let busPathname_array = [];
-      let busPathID_array = [];
-
-      //가는 버스들 번호 -> 버스id로 버스마다 가는 경로들 가져오기
-      for (let i = 0; i < busPath.result.path[0].subPath[1].lane.length; i++) {
-        const busPathname = busPath.result.path[0].subPath[1].lane[i]["busNo"];
-        const busPathId = busPath.result.path[0].subPath[1].lane[i]["busID"];
-        busPathname_array.push(busPathname);
-        busPathID_array.push(busPathId);
-      }
-
-      let $totalTime = document.getElementsByClassName("totalTime");
-      let $totalWalk = document.getElementsByClassName("totalWalk");
-      let $payment = document.getElementsByClassName("payment");
-      let $totalDistance = document.getElementsByClassName("totalDistance");
-      let $firstStartStation =
-        document.getElementsByClassName("firstStartStation");
-      let $lastEndStation = document.getElementsByClassName("lastEndStation");
-      let $busNo = document.getElementsByClassName("busNo");
-      // let otherbus = document.getElementsByClassName('otherbus');
-      let firstwalkdistance =
-        document.getElementsByClassName("firstwalkdistance");
-      let stationName = document.getElementsByClassName("stationName");
-      let secondtwalkdistance = document.getElementsByClassName(
-        "secondtwalkdistance"
-      );
+      const $toggleBtn = "<img\n" +
+          "                  src=\"img/down-arrow.png\"\n" +
+          "                  width=\"14rem\"\n" +
+          "                  height=\"14rem\"\n" +
+          "                  type=\"button\"\n" +
+          "                  data-toggle=\"collapse\"\n" +
+          "                  data-target=\".stationName\"\n" +
+          "                  style=\" padding-left: 1px\"\n" +
+          "          >"
 
       $(".totalTime").html(busPath.result.path[0].info.totalTime + "분");
       $(".totalWalk").html(
-        "도보" + busPath.result.path[0].info.totalWalk + "m"
+          "도보" + busPath.result.path[0].info.totalWalk + "m"
       );
       $(".payment").html(busPath.result.path[0].info.payment + "원");
       $(".totalDistance").html(busPath.result.path[0].info.totalDistance + "m");
 
       //버스 번호 가져올때 노선 같은 버스 다 표기하기
       $(".busNo").html(
-        busPath.result.path[0].subPath[1].lane[0]["busNo"] + "번 버스"
+          busPath.result.path[0].subPath[1].lane[0]["busNo"] + "번 버스"
       );
       $(".firstStartStation").html(
-        busPath.result.path[0].info.firstStartStation
+          busPath.result.path[0].info.firstStartStation
       );
       $(".lastEndStation").html(busPath.result.path[0].info.lastEndStation);
 
@@ -262,29 +246,110 @@ function searchBusLaneAJAX() {
       if (busPath.result.path[0].subPath[1].lane.length == 1) {
         $(".otherbus").html("대체 버스 정보가 없습니다.");
       } else {
-        // $('.otherbus').html(busPath.result.path[0].subPath[1].lane[i].busNo+",");
-        for (let i in busPath.result.path[0].subPath[1].lane) {
+        let subPath = busPath.result.path[0].subPath[1].lane
+        subPath.shift()
+        for (let i in subPath) {
           busPathname1 +=
-            busPath.result.path[0].subPath[1].lane[i]["busNo"] + ",";
+              subPath[i]["busNo"] + ",";
         }
         $(".otherbus").html(busPathname1);
       }
+      let stations = busPath.result.path[0].subPath[1].passStopList.stations
+      //출발지 제거
+      stations.shift()
+      //도착지 제거
+      stations.pop()
 
-      for (let i in busPath.result.path[0].subPath[1].passStopList.stations) {
-        stationName1 +=
-          busPath.result.path[0].subPath[1].passStopList.stations[i]
-            .stationName + "->";
+      const stationNum = stations.length + 1;
+      const stationNumNode = `<span style="font-size: 1rem; color: rgba(0,0,0,0.71); padding-left: 7px;">${stationNum}개 정류장 이동</span>` + $toggleBtn
+      $(".howManyStation").html(stationNumNode);
+
+      console.log(stations)
+      for (let i in stations) {
+        stationName1 += `<div style="padding-left: 15px;">${stations[i].stationName}</div>`
       }
       $(".stationName").html(stationName1);
 
       $(".firstwalkdistance").html(
-        "도보" + busPath.result.path[0].subPath[0].distance + "m"
+          "도보" + busPath.result.path[0].subPath[0].distance + "m"
       );
       $(".secondtwalkdistance").html(
-        "도보" + busPath.result.path[0].subPath[2].distance + "m"
+          "도보" + busPath.result.path[0].subPath[2].distance + "m"
       );
 
       //노선에 넣을 정류장 이름,       //
     }
   };
+}
+
+import pathTest from "../data/pathTest.js"
+
+//테스트 정보로 UI 테스트
+function busPathTest() {
+  const busPath = pathTest;
+  const $toggleBtn = "<img\n" +
+      "                  src=\"img/down-arrow.png\"\n" +
+      "                  width=\"14rem\"\n" +
+      "                  height=\"14rem\"\n" +
+      "                  type=\"button\"\n" +
+      "                  data-toggle=\"collapse\"\n" +
+      "                  data-target=\".stationName\"\n" +
+      "                  style=\" padding-left: 1px\"\n" +
+      "          >"
+
+  $(".totalTime").html(busPath.result.path[0].info.totalTime + "분");
+  $(".totalWalk").html(
+      "도보" + busPath.result.path[0].info.totalWalk + "m"
+  );
+  $(".payment").html(busPath.result.path[0].info.payment + "원");
+  $(".totalDistance").html(busPath.result.path[0].info.totalDistance + "m");
+
+  //버스 번호 가져올때 노선 같은 버스 다 표기하기
+  $(".busNo").html(
+      busPath.result.path[0].subPath[1].lane[0]["busNo"] + "번 버스"
+  );
+  $(".firstStartStation").html(
+      busPath.result.path[0].info.firstStartStation
+  );
+  $(".lastEndStation").html(busPath.result.path[0].info.lastEndStation);
+
+  let otherbus = document.getElementsByClassName("otherbus");
+  let busPathname1 = " "; //string으로 대체 버스번호들 저장
+  let stationName1 = " "; //string으로 노선 저장
+
+  if (busPath.result.path[0].subPath[1].lane.length == 1) {
+    $(".otherbus").html("대체 버스 정보가 없습니다.");
+  } else {
+    let subPath = busPath.result.path[0].subPath[1].lane
+    subPath.shift()
+    for (let i in subPath) {
+      busPathname1 +=
+          subPath[i]["busNo"] + ",";
+    }
+    $(".otherbus").html(busPathname1);
+  }
+  let stations = busPath.result.path[0].subPath[1].passStopList.stations
+  //출발지 제거
+  stations.shift()
+  //도착지 제거
+  stations.pop()
+
+  const stationNum = stations.length + 1;
+  const stationNumNode = `<span style="font-size: 1rem; color: rgba(0,0,0,0.71); padding-left: 7px;">${stationNum}개 정류장 이동</span>` + $toggleBtn
+  $(".howManyStation").html(stationNumNode);
+
+  console.log(stations)
+  for (let i in stations) {
+    stationName1 += `<div style="padding-left: 15px;">${stations[i].stationName}</div>`
+  }
+  $(".stationName").html(stationName1);
+
+  $(".firstwalkdistance").html(
+      "도보" + busPath.result.path[0].subPath[0].distance + "m"
+  );
+  $(".secondtwalkdistance").html(
+      "도보" + busPath.result.path[0].subPath[2].distance + "m"
+  );
+
+  //노선에 넣을 정류장 이름,       //
 }
