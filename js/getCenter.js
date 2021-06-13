65e636f322136c72cfc293fae2e8c2b2d9150505
유은지 <aine0227@naver.com>
2021-06-13 23:54:59 +0900

    확인 버튼 url 수정

--- js/getCenter.js
+++ js/getCenter.js
@@ -2,7 +2,7 @@ export default function getCenter(markers) {
     //중간위치 입력안되면 alert창 띄우기
     if (markers === null) {
         alert("중간거리가 없거나 주소를 입력하지 않으셨습니다. \n중간위치를 먼저 입력해주세요!");
-        document.location.href = "../about.html";
+        window.location.href = 'https://hyunjunyong.github.io/middle-location-pg/about.html';
     }else {
         const count = markers.length;
         const centerLon = markers.reduce(((p, c) => p + c.Ma), 0) / count;
