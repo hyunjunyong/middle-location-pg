4175b17a76776b28efc6628662e47ece1b21c9e9
유은지 <aine0227@naver.com>
2021-06-14 00:37:00 +0900

    마커 이미지 옵션 수정#3

--- js/contact.js
+++ js/contact.js
@@ -53,7 +53,7 @@ function bus() {
         var imageSrc =
             "https://github.com/hyunjunyong/middle-location-pg/blob/main/img/pin (1).png?raw=true", // 마커이미지의 주소입니다
           imageSize = new kakao.maps.Size(52, 58), // 마커이미지의 크기입니다
-          imageOption = { offset: new kakao.maps.Point(30,55) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+          imageOption = { offset: new kakao.maps.Point(28,57) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
         // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
         var markerImage = new kakao.maps.MarkerImage(
