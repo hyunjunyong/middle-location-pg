3edc59c6f778718e8690517498347fc09db1c890
유은지 <aine0227@naver.com>
2021-06-14 00:29:38 +0900

    마커 이미지 옵션 수정#2

--- js/contact.js
+++ js/contact.js
@@ -53,7 +53,7 @@ function bus() {
         var imageSrc =
             "https://github.com/hyunjunyong/middle-location-pg/blob/main/img/pin (1).png?raw=true", // 마커이미지의 주소입니다
           imageSize = new kakao.maps.Size(52, 58), // 마커이미지의 크기입니다
-          imageOption = { offset: new kakao.maps.Point(50,50) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+          imageOption = { offset: new kakao.maps.Point(30,55) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
         // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
         var markerImage = new kakao.maps.MarkerImage(
@@ -135,7 +135,7 @@ function bus() {
 bus();
 //api 작동
 for (let i = 0; i < positions.length; i++) {
-  const colors = ["#FF7F00", "#FFFF00", "#ffc300", "#80FF00", "#00FFFF","#0000FF","#8000FF","#FF00FF"];
+  const colors = ["#FF7F00", "#d42424", "#30ffe7", "#80FF00", "#00FFFF","#0000FF","#8000FF","#FF00FF"];
   let selecetedColor = colors[i];
   var request = new XMLHttpRequest();
 
