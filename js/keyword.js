df48dd2d1c4af091c2a91ec19d7a38b6bc22eeec
hyunjunyong <hjy4649@naver.com>
2021-06-14 21:24:46 +0900

    수정

--- js/keyword.js
+++ js/keyword.js
@@ -43,15 +43,15 @@ function searchPlaces() {
     let coord = new kakao.maps.LatLng(middlelon, middlelat);
     let callback = function (result, status) {
       if (status === kakao.maps.services.Status.OK) {
-        var keyword = (document.getElementById("keyword").value =
-          result[0].address.address_name + " 맛집");
+        $(document).ready(function () {
+          $("#keyword").val(result[0].address.address_name + " 맛집");
+        });
+        // var keyword = (document.getElementById("keyword").value =
+        //   result[0].address.address_name + " 맛집");
         if (!keyword.replace(/^\s+|\s+$/g, "")) {
           alert("키워드를 입력해주세요!");
           return false;
         }
-        $("#keyword").on("change keyup paste", function () {
-          console.log(keyword);
-        });
 
         // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
         ps.keywordSearch(keyword, placesSearchCB);
