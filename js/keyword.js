401cb5e7cc16c051adcaffde4334e4c4c209d7f2
hyunjunyong <hjy4649@naver.com>
2021-06-14 21:28:47 +0900

    ㅁ

--- js/keyword.js
+++ js/keyword.js
@@ -45,13 +45,13 @@ function searchPlaces() {
       if (status === kakao.maps.services.Status.OK) {
         $(document).ready(function () {
           $("#keyword").val(result[0].address.address_name + " 맛집");
+          if (!$("keyword").replace(/^\s+|\s+$/g, "")) {
+            alert("키워드를 입력해주세요!");
+            return false;
+          }
         });
         // var keyword = (document.getElementById("keyword").value =
         //   result[0].address.address_name + " 맛집");
-        if (!keyword.replace(/^\s+|\s+$/g, "")) {
-          alert("키워드를 입력해주세요!");
-          return false;
-        }
 
         // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
         ps.keywordSearch(keyword, placesSearchCB);
